0899e7b60d16012491393e3d72d1146486a69bce
ikostan <igorkostan@gmail.com>
2019-05-29 21:36:41 -0700

    Webapp first webpage

--- webapp/views.py
+++ webapp/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h2>HEY!</h2>")
+    return HttpResponse("<h2>This is Webapp web page!</h2>")
 
 
