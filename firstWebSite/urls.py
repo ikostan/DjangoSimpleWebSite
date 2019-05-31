8efd3743e62b857df8ea49366342eb7a2a9d83e0
ikostan <igorkostan@gmail.com>
2019-05-31 13:51:13 -0700

    webapp removed

--- firstWebSite/urls.py
+++ firstWebSite/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
+
 
 urlpatterns = [
+    url(r'^$', include('personal.urls')),  # Home web page
     path('admin/', admin.site.urls),
-    path('webapp/', include('webapp.urls')),
-    path('personal/', include('personal.urls')),
+    #path('webapp/', include('webapp.urls')),
+    #path('personal/', include('personal.urls')),
 ]
--- personal/urls.py
+++ personal/urls.py
@@ -3,3 +3,10 @@ from . import views
 
 
 urlpatterns = [url(r'^$', views.index, name='index')]
+
+
+#from django.conf.urls import re_path
+#from . import views
+
+#urlpatterns = [
+#    re_path('', views.index, name='index')]
