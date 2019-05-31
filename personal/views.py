f32fc4d5f0670ca20895c91e24b911690b89eb63
ikostan <igorkostan@gmail.com>
2019-05-31 15:36:02 -0700

    contact view creaated

--- personal/views.py
+++ personal/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'personal/home.html')
+
+
+def contact(request):
+    return render(request, 'personal/basic.html')
